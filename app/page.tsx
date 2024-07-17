fc4c5c2e878060be15f7cac4486d881eb5e6e99b
Aamir Hafiez <aamirhafiez@gmail.com>
2024-07-17 21:53:20 +0530

    removed console.log

--- app/page.tsx
+++ app/page.tsx
@@ -6,8 +6,6 @@ import useHomeController from "./view-model/useHome.viewModel";
 export default function Home() {
   const { data: storyData, isLoading } = useHomeController();
 
-  console.log("process.env", process.env);
-
   return (
     <div>
       <Story data={storyData} isLoading={isLoading} />
